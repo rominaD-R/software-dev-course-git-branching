0e0dfdb4e40feb958449f041c555257542f56c92
Romina Diaz-Rivero <romina@wustl.edu>
2025-11-26 14:35:33 -0600

    merge conflict 01

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// Rome Diaz-Rivero - 11/26/2025
+// This application prints "Hello, World!" and the numbers 0 to 9, along with their squares.
+
 console.log("Hello world!");
 
 for (let i = 0; i < 10; i++) {
